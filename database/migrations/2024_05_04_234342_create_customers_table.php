7a815a754417bfd1973bc7e7bb47ad308120580b
jonatamsr <jonatamsr@gmail.com>
2024-05-04 21:11:38 -0300

    creating migrations

---
+++ database/migrations/2024_05_04_234342_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('customer_type_id');
+            $table->string('name', 100);
+            $table->string('cpf_cnpj', 14);
+            $table->string('email', 100);
+            $table->string('password', 64);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('customer_type_id')->references('id')->on('customer_types');
+            $table->unique([
+                'cpf_cnpj',
+                'email',
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
